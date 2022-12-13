ed28e1e3d384e8f412c8c042a1ba296005c597fe
or <orshalom204@gmail.com>
2022-12-13 18:35:26 +0200

    Chnages before going abroad

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -10,10 +11,18 @@ public class GameManager : MonoBehaviour
     public ButtonVR button;
     public DisturbancesManager disturbances;
     string lettersData = "vcda";
+    int lettersDelay;
+    float time;
+    List<float> PressedAndshould;
+    List<float> PressedAndshouldNot;
+    List<float> NotPressedAndshould;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        lettersDelay = 1000;
         board.text = "Welcome to Virtual Classroom\n Press the button when you are ready!";
         button.ButtonPress.AddListener(StartGameAsync);
     }
@@ -21,19 +30,42 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public async void StartGameAsync()
     {
         button.ButtonPress.RemoveListener(StartGameAsync);
         board.text = "Lets Start...";
-        await Task.Delay(2000);
+        await Task.Delay(4000);
         disturbances.StartDisturbances(1f);
-        foreach (char c in lettersData)
+        bool pressed = false;
+        button.ButtonPress.AddListener(() =>
         {
-            board.text = c.ToString();
-            await Task.Delay(1000);
+            pressed = true;
+        });
+        for (int i = 0; i < lettersData.Length; i++)
+        {
+            pressed = false;
+            board.text = lettersData[i].ToString();
+            await Task.Delay(lettersDelay);
+            if (pressed && ShouldPress(i))
+            {
+                time = (i * lettersDelay) / (60 / 1000);
+                PressedAndshould.Add(time);
+            }
+            if (pressed && !ShouldPress(i))
+            {
+               time = (i * lettersDelay) / (60 / 1000);
+                PressedAndshouldNot.Add(time);
+            }
+            if(!pressed && ShouldPress(i))
+            {
+                time = (i * lettersDelay) / (60 / 1000);
+                NotPressedAndshould.Add(time);
+            }
+
+
         }
         disturbances.Stop();
         board.text = "U R DONE";
@@ -44,6 +76,10 @@ public class GameManager : MonoBehaviour
         board.text = times;
     }
 
+    private bool ShouldPress(int i)
+    {
+        return i != 0 && lettersData[i] == 'x' && lettersData[i - 1] == 'a';
+    }
 
     IEnumerator WaitFor(int seconds)
     {
